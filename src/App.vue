d3661a5b203fb29be10f814bfaaa833347b80aa1
Chaoying <1754129@gmail.com>
2022-10-01 16:03:03 +0800

    feat: 定义组件的props属性

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Student name="林超赢" gender="男" :age="23"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Student from './components/Student.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Student
+}
 }
 </script>
 
---
+++ src/components/Student.vue
@@ -0,0 +1,36 @@
+<template lang="">
+    <div>
+        <h1>{{msg}}</h1>
+        <h2>姓名：{{name}}</h2>
+        <h2>性别：{{gender}}</h2>
+        <h2>年龄：{{age+1}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Studeng',
+    data() {
+        return {
+            msg:"我是一名同济大学的学生！"
+        }
+    },
+    props: {
+        name: {
+            tyep: String,
+            required:true
+        },
+        age: {
+            tyep: Number,
+            required: true,
+            default:18
+        },
+        gender: {
+            tyep: String,
+            required:false
+        },
+    }
+}
+</script>
+<style lang="">
+    
+</style>
